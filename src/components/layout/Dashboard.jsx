649e6c355d34175d3cbee269a7fd23a82e2fb703
Juan camilo <61300748+bichozx@users.noreply.github.com>
2025-10-27 21:11:40 -0500

    se limpia el codigo
    se limpia el codigo

--- src/components/layout/Dashboard.jsx
+++ src/components/layout/Dashboard.jsx
@@ -21,35 +21,3 @@ export const Dashboard = () => {
     </div>
   );
 };
-
-// import { Navigate, Outlet } from 'react-router-dom';
-// import React, { useState } from 'react';
-
-// import { Footer } from './Footer';
-// import { Sidebar } from './SIdeBar';
-// import { useAuth } from '../../hooks/useAuth';
-
-// export const Dashboard = () => {
-//   const { user } = useAuth();
-//   const [sidebarOpen, setSidebarOpen] = useState(false);
-
-//   if (!user) return <Navigate to="/login" />;
-
-//   return (
-//     <div className="d-flex flex-column flex-md-row min-vh-100">
-//       {/* Sidebar colapsable en móviles */}
-//       <Sidebar isOpen={sidebarOpen} toggleSidebar={() => setSidebarOpen(!sidebarOpen)} />
-
-//       {/* Contenido principal */}
-//       <div className="flex-grow-1 d-flex flex-column bg-light">
-//         <main className="flex-grow-1 p-3 p-md-4">
-//           <Outlet />
-//         </main>
-//         <Footer />
-//       </div>
-//     </div>
-//   );
-// };
-
-
-
